2cd6a7152239ec7551eef0d32ec11971e42f88e9
xuming <xumingthepoet@gmail.com>
2026-05-21 04:23:35 +0800

    Split StoryState flow state helpers

--- crates/ink-runtime/src/story_state.rs
+++ crates/ink-runtime/src/story_state.rs
@@ -2,15 +2,13 @@ use std::{cell::RefCell, rc::Rc};
 
 use crate::{
     callstack::CallStack,
-    choice::Choice,
     container::Container,
     flow::Flow,
     object::RTObject,
-    path::Path,
     pointer::{self, Pointer},
     push_pop::PushPopType,
     state_patch::StatePatch,
-    story::{Story, INK_VERSION_CURRENT},
+    story::INK_VERSION_CURRENT,
     story_error::StoryError,
     variables_state::VariablesState,
 };
@@ -24,6 +22,7 @@ static DEFAULT_FLOW_NAME: &str = "DEFAULT_FLOW";
 
 mod errors;
 mod evaluation_stack;
+mod flow_state;
 mod function_eval;
 mod output;
 mod output_mutation;
@@ -77,67 +76,8 @@ impl StoryState {
         state
     }
 
-    pub fn can_continue(&self) -> bool {
-        !self.get_current_pointer().is_null() && !self.has_error()
-    }
-
-    /// String representation of the location where the story currently is.
-    pub fn current_path_string(&self) -> Option<String> {
-        let pointer = self.get_current_pointer();
-        pointer.get_path().map(|path| path.to_string())
-    }
-
-    pub fn get_current_pointer(&self) -> Pointer {
-        self.get_callstack()
-            .borrow()
-            .get_current_element()
-            .current_pointer
-            .clone()
-    }
-
     pub fn get_callstack(&self) -> &Rc<RefCell<CallStack>> {
         &self.current_flow.callstack
-    }
-
-    pub fn set_did_safe_exit(&mut self, did_safe_exit: bool) {
-        self.did_safe_exit = did_safe_exit;
-    }
-
-    pub fn get_generated_choices_mut(&mut self) -> &mut Vec<Rc<Choice>> {
-        &mut self.current_flow.current_choices
-    }
-
-    pub fn get_generated_choices(&self) -> &Vec<Rc<Choice>> {
-        &self.current_flow.current_choices
-    }
-
-    pub fn is_did_safe_exit(&self) -> bool {
-        self.did_safe_exit
-    }
-
-    pub fn set_current_pointer(&self, pointer: Pointer) {
-        self.get_callstack()
-            .as_ref()
-            .borrow_mut()
-            .get_current_element_mut()
-            .current_pointer = pointer;
-    }
-
-    pub fn set_previous_pointer(&self, p: Pointer) {
-        self.get_callstack()
-            .as_ref()
-            .borrow_mut()
-            .get_current_thread_mut()
-            .previous_pointer = p.clone();
-    }
-
-    pub fn get_previous_pointer(&self) -> Pointer {
-        self.get_callstack()
-            .as_ref()
-            .borrow_mut()
-            .get_current_thread_mut()
-            .previous_pointer
-            .clone()
     }
 
     pub fn pop_callstack(&mut self, t: Option<PushPopType>) -> Result<(), StoryError> {
@@ -161,17 +101,6 @@ impl StoryState {
             .borrow_mut()
             .get_current_element_mut()
             .current_pointer = Pointer::start_of(self.main_content_container.clone())
-    }
-
-    pub fn get_current_choices(&self) -> Option<&Vec<Rc<Choice>>> {
-        // If we can continue generating text content rather than choices,
-        // then we reflect the choice list as being empty, since choices
-        // should always come at the end.
-        if self.can_continue() {
-            return None;
-        }
-
-        Some(&self.current_flow.current_choices)
     }
 
     pub fn copy_and_start_patching(&self, for_background_save: bool) -> StoryState {
@@ -255,41 +184,6 @@ impl StoryState {
         self.variables_state.apply_patch();
 
         self.patch = None;
-    }
-
-    pub fn set_diverted_pointer(&mut self, p: Pointer) {
-        self.diverted_pointer = p;
-    }
-
-    pub fn set_chosen_path(
-        &mut self,
-        path: &Path,
-        incrementing_turn_index: bool,
-    ) -> Result<(), StoryError> {
-        // Changing direction, assume we need to clear current set of choices
-        self.current_flow.current_choices.clear();
-
-        let mut new_pointer = Story::pointer_at_path(&self.main_content_container, path)?;
-        if !new_pointer.is_null() && new_pointer.index == -1 {
-            new_pointer.index = 0;
-        }
-
-        self.set_current_pointer(new_pointer);
-
-        let _ = incrementing_turn_index;
-
-        Ok(())
-    }
-
-    pub(crate) fn force_end(&mut self) {
-        self.get_callstack().borrow_mut().reset();
-
-        self.current_flow.current_choices.clear();
-
-        self.set_current_pointer(pointer::NULL.clone());
-        self.set_previous_pointer(pointer::NULL.clone());
-
-        self.set_did_safe_exit(true);
     }
 
     pub fn to_json(&self) -> Result<String, StoryError> {
